397c9bf9cc5894d67805637890de6f6efdfc334f
Alexander <alexander0153@users.noreply.github.com>
2020-06-09 18:35:42 +0200

    Fix audit log issues

--- src/commonMain/kotlin/com/jessecorbett/diskord/api/model/AuditLog.kt
+++ src/commonMain/kotlin/com/jessecorbett/diskord/api/model/AuditLog.kt
@@ -31,13 +31,13 @@ data class AuditLogChange(
 
 @Serializable
 data class OptionalEntryData(
-    @SerialName("delete_member_days") val pruneKickedAfterDays: String,
-    @SerialName("members_removed") val pruneMembersPrunedCount: String,
-    @SerialName("channel_id") val deleteChannelId: String,
-    @SerialName("count") val deleteMessageCount: String,
-    @SerialName("id") val overwriteEntityId: String,
-    @SerialName("type") val overwriteEntityType: OverwrittenEntityType,
-    @SerialName("role_name") val overwriteRoleName: String
+    @SerialName("delete_member_days") val pruneKickedAfterDays: String? = null,
+    @SerialName("members_removed") val pruneMembersPrunedCount: String? = null,
+    @SerialName("channel_id") val deleteChannelId: String? = null,
+    @SerialName("count") val deleteMessageCount: String? = null,
+    @SerialName("id") val overwriteEntityId: String? = null,
+    @SerialName("type") val overwriteEntityType: OverwrittenEntityType? = null,
+    @SerialName("role_name") val overwriteRoleName: String? = null
 )
 
 @Serializable
